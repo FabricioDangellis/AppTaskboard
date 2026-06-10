cdc53652c8731c8525c16d06808236b5ef9f1c7a
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2026-06-10 15:00:21 -0300

    ADD: Adicionando a função para criar um usuário ao formuçário de cadastro

--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -1,14 +1,15 @@
 import { database } from "./db";
 
 export async function initializeDatabase() {
+
   await database.execAsync(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL,
-      created_at TEXT DEFAULT CURRENT_TIMESTAMP
-      deleted_at TEXT DEFAULT NULL,
+      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+      deleted_at TEXT DEFAULT NULL
     );
   `);
 
