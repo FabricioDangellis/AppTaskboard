62fc77fdaca855846b40b6e40ccfad4b6385a771
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2026-06-10 09:38:23 -0300

    ADD: Adicionando o service de Task

---
+++ src/services/TaskService.ts
@@ -0,0 +1,109 @@
+import { CreateTaskDTO } from "../@types/task/CreateTaskDTO";
+import { UpdateTaskDTO } from "../@types/task/UpdateTaskDTO";
+import { TaskRepository } from "../database/repositories/TaskRepository";
+
+export class TaskService {
+    private taskRepository: TaskRepository;
+
+    constructor() {
+        this.taskRepository = new TaskRepository();
+    }
+
+    async create({userId, title, description, flag}: CreateTaskDTO){
+        if (!title.trim()) {
+            throw new Error("O título da tarefa é obrigatório.");
+        }
+
+        return await this.taskRepository.create(
+            userId,
+            title,
+            description,
+            flag, 
+        );
+    }
+
+    async findById(id: number) {
+        const task = await this.taskRepository.findById(id);
+
+        if (!task) {
+            throw new Error("Tarefa não encontrada.");
+        }
+
+        return task;
+    }
+
+    async findAllByUser(userId: number) {
+        return await this.taskRepository.findAllByUser(
+            userId,
+        );
+    }
+
+    async findCompletedByUser(userId: number) {
+        return await this.taskRepository.findCompletedByUser(
+            userId,
+        );
+    }
+
+    async findPendingByUser(userId: number) {
+        return await this.taskRepository.findPendingByUser(
+            userId,
+        );
+    }
+
+    async update({id, title, description, flag}: UpdateTaskDTO) {
+        const task = await this.taskRepository.findById(
+            id
+        );
+
+        if (!task) {
+            throw new Error("Tarefa não encontrada.");
+        }
+
+        if (!title.trim()) {
+            throw new Error("O título da tarefa é obrigatório.");
+        }
+
+        await this.taskRepository.update(
+            id,
+            title,
+            description,
+            flag,
+        );
+    }
+
+    async complete(id: number) {
+        const task = await this.taskRepository.findById(
+            id,
+        );
+
+        if (!task) {
+            throw new Error("Tarefa não encontrada.");
+        }
+
+        await this.taskRepository.complete(id);
+    }
+
+    async reopen(id: number) {
+        const task = await this.taskRepository.findById(
+        id,
+        );
+
+        if (!task) {
+            throw new Error("Tarefa não encontrada.");
+        }
+
+        await this.taskRepository.reopen(id);
+    }
+
+    async delete(id: number) {
+        const task = await this.taskRepository.findById(
+            id
+        );
+
+        if (!task) {
+            throw new Error("Tarefa não encontrada.");
+        }
+
+        await this.taskRepository.delete(id);
+    }
+}
